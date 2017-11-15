73a53ddf28e32bb38ad31977c3f7f2376238cb6c
Yu Xuan Wu <ywu148@binghamton.edu>
2017-11-15 16:36:28 -0500

    Added support for electron package builder, added nav bar and card styling

--- electron_new_gui/main.js
+++ electron_new_gui/main.js
@@ -2,16 +2,15 @@ const electron = require("electron")
 const url = require("url")
 const path = require("path")
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
+//HTML windows
 let main_window;
 
 app.on("ready", function(){
 
     //Create new window
-    main_window = new BrowserWindow({
-        frame : false
-    });
+    main_window = new BrowserWindow({});
 
     //Load HTML into window
     main_window.loadURL(url.format({
@@ -20,10 +19,41 @@ app.on("ready", function(){
         slashes: true
     }));
 
-    main_window.toggleDevTools();
-
     //Handler when "main_window" is closed
     main_window.on("closed", function(){
         app.quit();
     });
+
+    //Build Menubar from template
+    const main_menu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(main_menu);
 });
+
+/*-----Menubar Template-----*/
+const mainMenuTemplate = [
+    {
+        //Debug Label
+        label: "Debug",
+        submenu: [
+            //Open Debug Tools Label
+            {
+                label: "Toggle Element Inspector",
+                accelerator: process.platform == "Darwin" ? "Command+D": "Ctrl+D",
+                click(){
+                    main_window.toggleDevTools();
+                }
+            },
+            {
+                label: "Quit",
+                accelerator: process.platform == "Darwin" ? "Command+Q" : "Ctrl+Q",
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+if(process.platform == "Darwin"){
+    mainMenuTemplate.unshift({});
+}
